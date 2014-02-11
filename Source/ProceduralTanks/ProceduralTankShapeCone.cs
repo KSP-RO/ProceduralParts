bc38ec62a2cfb32f57d5755aaef2e6a41321d8e8
Swamp Ig <kerbal@ninjateaparty.com>
2014-02-11 22:07:26 +0800

    Massive commit
    There's so much done in this commit that it would be pointless to list
    it all.

---
+++ Source/ProceduralTanks/ProceduralTankShapeCone.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSPAPIExtensions;
+
+public class ProceduralTankShapeCone : AbstractSurfaceOfRevolutionShape
+{
+
+    [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Top", guiFormat = "F3"), UI_FloatEdit(minValue = 0.25f, maxValue = 10.0f, incrementLarge = 1.25f, incrementSmall = 0.25f, incrementSlide = 0.001f)]
+    public float topDiameter = 1.25f;
+    private float oldTopDiameter;
+
+    [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Bottom", guiFormat = "F3"), UI_FloatEdit(minValue = 0.25f, maxValue = 10.0f, incrementLarge = 1.25f, incrementSmall = 0.25f, incrementSlide = 0.001f)]
+    public float bottomDiameter = 1.25f;
+    private float oldBottomDiameter;
+
+    [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Length", guiFormat = "F3"), UI_FloatEdit(minValue = 0.25f, maxValue = 10.0f, incrementLarge = 1.25f, incrementSmall = 0.25f, incrementSlide = 0.001f)]
+    public float length = 1f;
+    private float oldLength;
+
+    public void Start()
+    {
+        if (!HighLogic.LoadedSceneIsEditor)
+            return;
+
+        UI_FloatEdit lengthEdit = (UI_FloatEdit)Fields["length"].uiControlEditor;
+        lengthEdit.maxValue = tank.lengthMax;
+        lengthEdit.minValue = tank.lengthMin;
+
+        UI_FloatEdit topDiameterEdit = (UI_FloatEdit)Fields["topDiameter"].uiControlEditor;
+        topDiameterEdit.maxValue = tank.diameterMax;
+        topDiameterEdit.minValue = tank.diameterMin;
+
+        UI_FloatEdit bottomDiameterEdit = (UI_FloatEdit)Fields["bottomDiameter"].uiControlEditor;
+        bottomDiameterEdit.maxValue = tank.diameterMax;
+        bottomDiameterEdit.minValue = tank.diameterMin;
+    }
+
+    protected override void UpdateShape(bool force)
+    {
+        if (!force && oldTopDiameter == topDiameter && oldBottomDiameter == bottomDiameter && oldLength == length)
+            return;
+
+        {
+            // Maxmin the volume.
+            float volume = (Mathf.PI * length * (topDiameter * topDiameter + topDiameter * bottomDiameter + bottomDiameter * bottomDiameter)) / 12f;
+            if (volume > tank.volumeMax)
+                volume = tank.volumeMax;
+            else if (volume < tank.volumeMin)
+                volume = tank.volumeMin;
+            else
+                goto nochange;
+
+            if (oldLength != length)
+                length = volume * 12f / (Mathf.PI * (topDiameter * topDiameter + topDiameter * bottomDiameter + bottomDiameter * bottomDiameter));
+            else if (oldTopDiameter != topDiameter) 
+            {
+                // this becomes solving the quadratic on topDiameter
+                float a = length * Mathf.PI;
+                float b = length * Mathf.PI * bottomDiameter;
+                float c = length * Mathf.PI * bottomDiameter * bottomDiameter - volume * 12f;
+
+                float det = Mathf.Sqrt(b * b - 4 * a * c);
+                topDiameter = (det - b) / ( 2f * a );
+            }
+            else
+            {
+                // this becomes solving the quadratic on bottomDiameter
+                float a = length * Mathf.PI;
+                float b = length * Mathf.PI * topDiameter;
+                float c = length * Mathf.PI * topDiameter * topDiameter - volume * 12f;
+
+                float det = Mathf.Sqrt(b * b - 4 * a * c);
+                bottomDiameter = (det - b) / (2f * a);
+            }
+        }
+        nochange:
+
+        // Perpendicular.
+        Vector2 norm = new Vector2(length, (bottomDiameter-topDiameter)/2f);
+        norm.Normalize();
+
+        WriteMeshes(
+            new ProfilePoint(topDiameter, 0.5f * length, 0f, norm),
+            new ProfilePoint(bottomDiameter, -0.5f * length, 1f, norm)
+            );
+
+        gameObject.SendMessage("UpdateSRBScale", bottomDiameter);
+
+        oldTopDiameter = topDiameter;
+        oldBottomDiameter = bottomDiameter;
+        oldLength = length;
+    }
+}
---
+++ Source/ProceduralTanks/TankContentSwitcher.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using KSPAPIExtensions;
+
+
+/// <summary>
+/// Module that allows switching the contents of a tank between different resources.
+/// The possible contents are set up in the config file, and the user may switch beween
+/// the different possiblities.
+/// 
+/// This is a bit of a light-weight version of RealFuels.
+/// 
+/// One can set this module up on any existing fuel tank (Maybe with module manager if you like) 
+/// if you set the tankVolume property in the config file.
+/// 
+/// The class also accepts the message UpdateTankVolume(float tankVolume) if attached to a dynamic resizing tank
+/// such as ProceeduralTanks.
+/// </summary>
+public class TankContentSwitcher : PartModule
+{
+    #region Callbacks
+
+    public override void OnStart(PartModule.StartState state)
+    {
+        InitializeTankType();
+    }
+
+    public void Update()
+    {
+        if (HighLogic.LoadedSceneIsEditor)
+            UpdateTankType();
+    }
+
+    public override void OnLoad(ConfigNode node)
+    {
+        if (HighLogic.LoadedScene != GameScenes.LOADING)
+            return;
+
+        tankTypeOptions = new List<TankTypeOption>();
+        foreach (ConfigNode optNode in node.GetNodes("TANK_TYPE_OPTION"))
+        {
+            TankTypeOption option = new TankTypeOption();
+            option.Load(optNode);
+            tankTypeOptions.Add(option);
+        }
+
+        // Unity for some daft reason, and not as according to it's own documentation, won't clone 
+        // serializable member fields. Lets DIY.
+        MemoryStream stream = new MemoryStream();
+        using (stream)
+        {
+            BinaryFormatter fmt = new BinaryFormatter();
+            fmt.Serialize(stream, tankTypeOptions);
+        }
+        tankTypeOptionsSerialized = stream.ToArray();
+    }
+
+    public override void OnAwake()
+    {
+        // Unity for some daft reason, and not as according to it's own documentation, won't clone 
+        // serializable member fields. Lets DIY.
+        if (tankTypeOptionsSerialized != null)
+        {
+            using (MemoryStream stream = new MemoryStream(tankTypeOptionsSerialized))
+            {
+                BinaryFormatter fmt = new BinaryFormatter();
+                tankTypeOptions = (List<TankTypeOption>)fmt.Deserialize(stream);
+            }
+        }
+    }
+
+
+    #endregion
+
+    #region Tank Volume
+
+    [KSPField]
+    public float volMultiplier = 1.0f; // for MFS
+
+    /// <summary>
+    /// Volume of tank in kilolitres. 
+    /// </summary>
+    [KSPField(guiActive = false, guiActiveEditor = true, guiName = "Volume", guiFormat = "F3", guiUnits = "kL")]
+    public float tankVolume = 0.0f;
+
+    [KSPField(guiActive = false, guiActiveEditor = true, guiName = "Dry Mass", guiFormat = "F2", guiUnits = "t")]
+    public float dryMass = 0.0f;
+
+    /// <summary>
+    /// Message sent from ProceduralTankShape when it updates.
+    /// </summary>
+    public void UpdateTankVolume(float tankVolume)
+    {
+        if (tankVolume <= 0f)
+            throw new ArgumentOutOfRangeException("tankVolume");
+
+        this.tankVolume = tankVolume;
+        UpdateMassAndResources(false);
+    }
+    
+    #endregion
+
+    #region Tank Type
+
+    [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = true, guiName = "Tank Type"), UI_ChooseOption]
+    public string tankType;
+
+    private TankTypeOption selectedTankType;
+
+    //[SerializeField]  <-- this doesn't work. Don't know why. Have given up trying to figure it out.
+    private List<TankTypeOption> tankTypeOptions;
+
+    // Serialize to bytes instead.
+    [SerializeField]
+    private byte[] tankTypeOptionsSerialized;
+
+    [Serializable]
+    public class TankTypeOption : IConfigNode
+    {
+        [Persistent]
+        public string optionName;
+        [Persistent]
+        public float dryDensity;
+        [Persistent]
+        public bool isStructural = false;
+        [Persistent]
+        public List<TankResource> resources;
+
+        public void Load(ConfigNode node)
+        {
+            ConfigNode.LoadObjectFromConfig(this, node);
+        }
+        public void Save(ConfigNode node)
+        {
+            ConfigNode.CreateConfigFromObject(this, node);
+        }
+    }
+
+    [Serializable]
+    public class TankResource : IConfigNode
+    {
+        [Persistent]
+        public string resourceName;
+        [Persistent]
+        public float unitsPerKL;
+        [Persistent]
+        public bool isTweakable = true;
+
+        public void Load(ConfigNode node)
+        {
+            ConfigNode.LoadObjectFromConfig(this, node);
+        }
+        public void Save(ConfigNode node)
+        {
+            ConfigNode.CreateConfigFromObject(this, node);
+        }
+    }
+
+    private void InitializeTankType()
+    {
+        if (tankTypeOptions == null || tankTypeOptions.Count == 0)
+            throw new InvalidOperationException("No tank type options available");
+
+        BaseField field = Fields["tankType"];
+        UI_ChooseOption options = (UI_ChooseOption)field.uiControlEditor;
+
+        options.options = tankTypeOptions.ConvertAll<string>(opt => opt.optionName).ToArray();
+
+        field.guiActiveEditor = (tankTypeOptions.Count > 1);
+
+        if (string.IsNullOrEmpty(tankType))
+            tankType = tankTypeOptions[0].optionName;
+    }
+
+    private void UpdateTankType()
+    {
+        if (tankTypeOptions == null || ( selectedTankType != null && selectedTankType.optionName == tankType))
+            return;
+
+        TankTypeOption oldTankType = selectedTankType;
+
+        selectedTankType = tankTypeOptions.Find(opt => opt.optionName == tankType);
+
+        if (selectedTankType == null)
+        {
+            if (oldTankType == null)
+            {
+                Debug.LogWarning("*TCS* Initially selected tank type '" + tankType + "' does not exist, reverting to default");
+                selectedTankType = tankTypeOptions[0];
+                tankType = selectedTankType.optionName;
+            }
+            else 
+            {
+                Debug.LogWarning("*TCS* Selected tank type '" + tankType + "' does not exist, reverting to previous");
+                selectedTankType = oldTankType;
+                tankType = selectedTankType.optionName;
+                return;
+            }
+        }
+
+        Fields["dryMass"].guiName = (selectedTankType.isStructural) ? "Mass" : "Dry Mass";
+
+        UpdateMassAndResources(true);
+    }
+
+    #endregion
+
+    #region Resources
+
+    private void UpdateMassAndResources(bool typeChanged)
+    {
+        // Wait for the first update...
+        if (selectedTankType == null)
+            return;
+
+        dryMass = part.mass = Mathf.Round(selectedTankType.dryDensity * tankVolume * volMultiplier * 1000f) / 1000f;
+
+        // TODO: breaking force
+        //part.breakingForce = 50f;
+        //part.breakingTorque = 50f;
+
+        // TODO: do we really want to update the breaking force?
+        //childAttachment.breakingForce = 969.47f * Mathf.Pow(massFactor, 0.3684f);
+        //childAttachment.breakingTorque = 969.47f * Mathf.Pow(massFactor, 0.3684f);
+
+
+        // Update the resources list.
+        UIPartActionWindow window = FindWindow();
+        if (window == null)
+            goto reinitialize;
+
+        if (!typeChanged) 
+        {
+            // Hopefully we can just fiddle with the existing part resources.
+            if (part.Resources.Count != selectedTankType.resources.Count)
+            {
+                Debug.LogWarning("*TCS* Selected and existing resource counts differ");
+                goto reinitialize;
+            }
+
+            for (int i = 0; i < part.Resources.Count; ++i)
+            {
+                PartResource partRes = part.Resources[i];
+                TankResource tankRes = selectedTankType.resources[i];
+
+                if (partRes.resourceName != tankRes.resourceName)
+                {
+                    Debug.LogWarning("*TCS* Selected and existing resource names differ");
+                    goto reinitialize;
+                }
+
+                double amount = (float)Math.Round(tankVolume * volMultiplier * tankRes.unitsPerKL, 2);
+                double oldFillFraction = partRes.amount / partRes.maxAmount;
+
+                partRes.maxAmount = amount;
+                partRes.amount = amount * oldFillFraction;
+
+                if (!UpdateWindow(window, partRes))
+                    goto reinitialize;
+            }
+            gameObject.SendMessage("UpdateTankResources");
+            return;
+        }
+
+        reinitialize:
+        
+        // Purge the old resources
+        foreach (PartResource res in part.Resources)
+            Destroy(res);
+        part.Resources.list.Clear();
+
+        // Build them afresh. This way we don't need to do all the messing around with reflection
+        // The downside is the UIPartActionWindow gets maked dirty and rebuit, so you can't use 
+        // the sliders that affect tank contents properly cos they get recreated underneith you and the drag dies.
+        foreach (TankResource res in selectedTankType.resources)
+        {
+            double amount = (double)Math.Round(tankVolume * volMultiplier * res.unitsPerKL, 2);
+
+            ConfigNode node = new ConfigNode("RESOURCE");
+            node.AddValue("name", res.resourceName);
+            node.AddValue("amount", amount);
+            node.AddValue("maxAmount", amount);
+            node.AddValue("isTweakable", res.isTweakable);
+            part.AddResource(node);
+        }
+
+        if (window != null)
+            window.displayDirty = true;
+
+        gameObject.SendMessage("UpdateTankResources");
+    }
+
+    #endregion
+
+    #region Nasty Reflection Code
+
+    private FieldInfo windowListField;
+    private FieldInfo actionItemListField;
+
+    private UIPartActionWindow FindWindow()
+    {
+        // We need to do quite a bit of piss-farting about with reflection to 
+        // dig the thing out.
+        UIPartActionController controller = UIPartActionController.Instance;
+
+        if(windowListField == null) 
+        {
+            Type cntrType = typeof(UIPartActionController);
+            foreach (FieldInfo info in cntrType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+            {
+                if (info.FieldType == typeof(List<UIPartActionWindow>))
+                {
+                    windowListField = info;
+                    goto foundField;
+                }
+            }
+            Debug.LogWarning("*TCS* Unable to find UIPartActionWindow list");
+            return null;
+        }
+    foundField:
+
+        foreach (UIPartActionWindow window in (List<UIPartActionWindow>)windowListField.GetValue(controller))
+            if (window.part == part)
+                return window;
+
+        return null;
+    }
+
+    private bool UpdateWindow(UIPartActionWindow window, PartResource res)
+    {
+        if (actionItemListField == null)
+        {
+            Type cntrType = typeof(UIPartActionWindow);
+            foreach (FieldInfo info in cntrType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+            {
+                if (info.FieldType == typeof(List<UIPartActionItem>))
+                {
+                    actionItemListField = info;
+                    goto foundField;
+                }
+            }
+            Debug.LogWarning("*TCS* Unable to find UIPartActionWindow list");
+            return false;
+        }
+    foundField:
+
+        UIPartActionResourceEditor editor;
+        foreach (UIPartActionItem item in (List<UIPartActionItem>)actionItemListField.GetValue(window))
+        {
+            UIPartActionResourceEditor ed = item as UIPartActionResourceEditor;
+            if (ed == null)
+                continue;
+            if (ed.resourceName.Text == res.resourceName)
+            {
+                editor = ed;
+                goto foundEditor;
+            }
+        }
+        Debug.LogWarning("*TCS* Unable to find resource editor in window");
+        return false;
+
+    foundEditor:
+
+        // Fortunatly as we're keeping proportions, we don't need to update the slider.
+        editor.resourceMax.Text = res.maxAmount.ToString("F2");
+        editor.resourceAmnt.Text = res.amount.ToString("F2");
+
+        return true;
+    }
+
+
+    #endregion
+}
