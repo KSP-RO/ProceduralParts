ef867d5befe31ed87f31cdb51d10370a081db0cf
Swamp Ig <kerbal@ninjateaparty.com>
2014-03-30 21:09:36 +0800

    Added assembly info

--- Source/ProceduralParts/Properties/AssemblyInfo.cs
+++ Source/ProceduralParts/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KSPAPIExtensions")]
-[assembly: AssemblyDescription("A set of utilities for app developers for Kerbal Space Program")]
+[assembly: AssemblyTitle("ProceduralParts")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KSPAPIExtensions")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("ProceduralParts.Properties")]
+[assembly: AssemblyCopyright("Chris Wood 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e46ed9d3-b992-4f9a-8e9a-64536116a2c4")]
+[assembly: Guid("a1c9118f-14a4-4aaf-bb43-87b1c13a7d4d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.*")]
+[assembly: AssemblyVersion("0.7.5.*")]
+[assembly: AssemblyFileVersion("0.7.5.*")]
