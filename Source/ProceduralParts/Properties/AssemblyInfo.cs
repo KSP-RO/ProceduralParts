d8c8d7e55c0056a8ad7ff7f330161743118c6d03
Swamp Ig <kerbal@ninjateaparty.com>
2014-04-11 08:07:29 +0800

    Patch up a few bits

--- Source/ProceduralParts/Properties/AssemblyInfo.cs
+++ Source/ProceduralParts/Properties/AssemblyInfo.cs
@@ -29,5 +29,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.1.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyVersion("0.8.1.*")]
