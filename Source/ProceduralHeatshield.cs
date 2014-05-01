3039e2b80f47d8d1b8b1a5fa0c344fd5ebe96578
Swamp Ig <kerbal@ninjateaparty.com>
2014-05-01 17:59:20 +0800

    Heatshield module

---
+++ Source/ProceduralHeatshield.cs
@@ -0,0 +1,53 @@
+﻿using KSPAPIExtensions.PartMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ProceduralParts
+{
+    /// <summary>
+    /// For heat shields. All this does is copies the top node size to the bottom.
+    /// </summary>
+    public class ProceduralHeatshield : PartModule
+    {
+        public override void OnAwake()
+        {
+            base.OnAwake();
+            PartMessageService.Register(this);
+        }
+
+        public override void OnStart(StartState state)
+        {
+            CopyNodeSizeAndStrength();
+        }
+
+        [KSPField]
+        public string bottomNodeId = "bottom";
+
+        [KSPField]
+        public string topNodeId = "top";
+
+        private AttachNode bottomNode;
+        private AttachNode topNode;
+        
+        [PartMessageListener(typeof(PartAttachNodeSizeChanged))]
+        public void PartAttachNodeSizeChanged(AttachNode node, float minDia, float area) 
+        {
+            CopyNodeSizeAndStrength();
+        }
+
+        private void CopyNodeSizeAndStrength()
+        {
+            if (bottomNode == null)
+                bottomNode = part.findAttachNode(bottomNodeId);
+            if (topNode == null)
+                topNode = part.findAttachNode(topNodeId);
+            bottomNode.size = topNode.size;
+            bottomNode.breakingForce = topNode.breakingForce;
+            bottomNode.breakingTorque = topNode.breakingTorque;
+        }
+
+    }
+}
