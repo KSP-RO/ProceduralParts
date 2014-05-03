333c82eb77673e252a8d28628b0514b453fcff58
Swamp Ig <kerbal@ninjateaparty.com>
2014-05-03 10:52:36 +0800

    Proper dissipation and loss model

--- Source/ProceduralHeatshield.cs
+++ Source/ProceduralHeatshield.cs
@@ -1,4 +1,5 @@
-﻿using KSPAPIExtensions.PartMessage;
+﻿using KSPAPIExtensions;
+using KSPAPIExtensions.PartMessage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +37,8 @@ namespace ProceduralParts
         public void PartAttachNodeSizeChanged(AttachNode node, float minDia, float area) 
         {
             CopyNodeSizeAndStrength();
+            if (GameSceneFilter.AnyEditor.IsLoaded())
+                UpdateDissipationAndLoss(minDia);
         }
 
         private void CopyNodeSizeAndStrength()
@@ -47,6 +50,32 @@ namespace ProceduralParts
             bottomNode.size = topNode.size;
             bottomNode.breakingForce = topNode.breakingForce;
             bottomNode.breakingTorque = topNode.breakingTorque;
+        }
+
+
+        private void UpdateDissipationAndLoss(float minDia)
+        {
+
+            Debug.LogWarning("Diameter: " + minDia);
+
+            // The heat model is going to change considerably.
+            // Will just do an unconfigurable quick and dirty way for now.
+            FloatCurve loss = new FloatCurve();
+            loss.Add(650, 0, 0, 0);
+            loss.Add(1000, (float)Math.Round(34.64794156 * Math.Pow(minDia, 2.75), 1));
+            loss.Add(3000, (float)Math.Round(43.30992695 * Math.Pow(minDia, 2.75), 1), 0, 0);
+
+            FloatCurve dissipation = new FloatCurve();
+            dissipation.Add(300, 0, 0, 0);
+            dissipation.Add(500, (float)Math.Round(562.5 / minDia / minDia, 1), 0, 0);
+
+            // Save it.
+            PartModule modHeatShield = part.Modules["ModuleHeatShield"];
+
+            Type type = modHeatShield.GetType();
+
+            type.GetField("loss").SetValue(modHeatShield, loss);
+            type.GetField("dissipation").SetValue(modHeatShield, dissipation);
         }
 
     }
