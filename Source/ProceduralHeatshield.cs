ceec5cd92efd971acda981ae6bbf684da5b02191
Swamp Ig <kerbal@ninjateaparty.com>
2014-05-15 19:45:27 +0800

    Update heatshield model

--- Source/ProceduralHeatshield.cs
+++ Source/ProceduralHeatshield.cs
@@ -22,6 +22,8 @@ namespace ProceduralParts
         public override void OnStart(StartState state)
         {
             CopyNodeSizeAndStrength();
+            if(minDia > 0)
+                UpdateDissipationAndLoss(minDia);
         }
 
         [KSPField]
@@ -30,12 +32,18 @@ namespace ProceduralParts
         [KSPField]
         public string topNodeId = "top";
 
+        [KSPField(isPersistant=true)]
+        public float minDia;
+
         private AttachNode bottomNode;
         private AttachNode topNode;
         
         [PartMessageListener(typeof(PartAttachNodeSizeChanged))]
         public void PartAttachNodeSizeChanged(AttachNode node, float minDia, float area) 
         {
+            if (node.id != topNodeId)
+                return;
+            this.minDia = minDia;
             CopyNodeSizeAndStrength();
             if (GameSceneFilter.AnyEditor.IsLoaded())
                 UpdateDissipationAndLoss(minDia);
@@ -55,20 +63,17 @@ namespace ProceduralParts
 
         private void UpdateDissipationAndLoss(float minDia)
         {
-
-            Debug.LogWarning("Diameter: " + minDia);
-
             // The heat model is going to change considerably.
             // Will just do an unconfigurable quick and dirty way for now.
             FloatCurve loss = new FloatCurve();
             loss.Add(650, 0, 0, 0);
-            loss.Add(1000, (float)Math.Round(34.64794156 * Math.Pow(minDia, 2.75), 1));
-            loss.Add(3000, (float)Math.Round(43.30992695 * Math.Pow(minDia, 2.75), 1), 0, 0);
+            loss.Add(1000, (float)Math.Round(40 * Math.Pow(minDia, 2), 1));
+            loss.Add(3000, (float)Math.Round(50 * Math.Pow(minDia, 2), 1), 0, 0);
 
             FloatCurve dissipation = new FloatCurve();
             dissipation.Add(300, 0, 0, 0);
-            dissipation.Add(500, (float)Math.Round(562.5 / minDia / minDia, 1), 0, 0);
-
+            dissipation.Add(500, (float)Math.Round(70 * Math.Pow(minDia, -2), 1), 0, 0);
+            
             // Save it.
             PartModule modHeatShield = part.Modules["ModuleHeatShield"];
 
@@ -76,6 +81,11 @@ namespace ProceduralParts
 
             type.GetField("loss").SetValue(modHeatShield, loss);
             type.GetField("dissipation").SetValue(modHeatShield, dissipation);
+
+            ConfigNode confNode = new ConfigNode();
+            loss.Save(confNode);
+
+            Debug.LogWarning("Updating float curves: minDia= " + minDia + "\n" + confNode);
         }
 
     }
