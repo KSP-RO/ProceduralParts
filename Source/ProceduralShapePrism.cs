1d2cca8bacf9fb09fe0d8a663c43a0403227c115
James Turner <james_w_turner27@hotmail.com>
2016-05-27 15:39:00 -0400

    completed prism mesh writing function

--- Source/ProceduralShapePrism.cs
+++ Source/ProceduralShapePrism.cs
@@ -23,23 +23,42 @@ namespace ProceduralParts
 
         [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Sides", guiFormat = "0"),
          UI_FloatEdit(scene = UI_Scene.Editor)]
-        public float sides = 6f;
+        public float sides = 4f;
         private float oldSides;
 
         [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Mode:"),
-         UI_Toggle(disabledText = "Inscribed", enabledText = "Circumscribed")]
+         UI_Toggle(disabledText = "Circumscribed", enabledText = "Inscribed")]
         public bool isInscribed = false;
         private bool oldIsInscribed;
 
         #endregion
+
+        public override void OnStart(StartState state)
+        {
+            UpdateTechConstraints();
+        }
 
         private static float GetRealOuterDiam(bool inscribed, float diam, int sides)
         {
             if (!inscribed)
                 return diam;
             float theta = (Mathf.PI * 2f) / (float)sides;
-            float radius = diam / 2f;
-            return 2f * (radius / Mathf.Cos(theta / 2f));
+            return diam / Mathf.Cos(theta / 2f);
+        }
+
+        private static float GetRealInnerDiam(bool inscribed, float diam, int sides)
+        {
+            if (inscribed)
+                return diam;
+            float theta = (Mathf.PI * 2f) / (float)sides;
+            return diam * Mathf.Cos(theta / 2f);
+        }
+
+        private static float GetSideLength(bool inscribed, float diam, int sides)
+        {
+            float theta = (Mathf.PI * 2f) / (float)sides;
+            var rad = GetRealInnerDiam(inscribed, diam, sides) / 2f;
+            return 2f * rad * Mathf.Tan(theta / 2f);
         }
 
         private float CalcVolume()
@@ -82,20 +101,23 @@ namespace ProceduralParts
                 oldIsInscribed == isInscribed)
                 return;
 
-            if (HighLogic.LoadedSceneIsFlight)
-            {
-                Volume = CalcVolume();
-            }
-            else if (HighLogic.LoadedSceneIsEditor)
-            {
+            Volume = CalcVolume();
 
+            Vector2 norm = new Vector2(1, 0);
+            var realDiam = GetRealOuterDiam(isInscribed, diameter, (int)sides);
 
-            }
+            var sideLen = GetSideLength(isInscribed, diameter, (int)sides);
+            
+            RaiseChangeTextureScale("sides", PPart.SidesMaterial, new Vector2(sideLen * sides * 2f, length));
 
-            //var realDiam = GetRealOuterDiam(isInscribed, diameter, (int)sides);
-            //var oldRealDiam = GetRealOuterDiam(oldIsInscribed, oldDiameter, (int)oldSides);
+            UpdateMeshNodesSizes(
+                new ProfilePoint(realDiam, -0.5f * length, 0f, norm),
+                new ProfilePoint(realDiam, 0.5f * length, 1f, norm)
+                );
 
-
+            var sideMesh = CreatePrismMesh((int)sides, diameter, length, isInscribed);
+            var endsMesh = CreatePismEnds((int)sides, diameter, length, isInscribed);
+            WriteMeshes(sideMesh, endsMesh, null);
 
             oldDiameter = diameter;
             oldLength = length;
@@ -113,6 +135,7 @@ namespace ProceduralParts
             var mesh = new UncheckedMesh(sides * 2 + 2, sides * 2);
             float theta = (Mathf.PI * 2f) / (float)sides;
             float radius = realDiam / 2;
+            var bOffset = sides + 1;
             //vertices/normals/tangents/uvs
             for (int s = 0; s <= sides; s++)
             {
@@ -124,19 +147,126 @@ namespace ProceduralParts
                 mesh.tangents[s] = new Vector4(-posZ, 0, posX, -1).normalized;
                 mesh.uv[s] = new Vector2((1f / (float)sides) * s, 0f);
                 //botom
-                mesh.verticies[s + sides + 1] = new Vector3(posX * radius, 0.5f * length, posZ * radius);
-                mesh.normals[s + sides + 1] = mesh.normals[s];
-                mesh.tangents[s + sides + 1] = mesh.tangents[s];
-                mesh.uv[s + sides + 1] = new Vector2((1f / (float)sides) * s, 0f);
+                mesh.verticies[s + bOffset] = new Vector3(posX * radius, 0.5f * length, posZ * radius);
+                mesh.normals[s + bOffset] = mesh.normals[s];
+                mesh.tangents[s + bOffset] = mesh.tangents[s];
+                mesh.uv[s + bOffset] = new Vector2((1f / (float)sides) * s, 1f);
             }
             //triangles
+            var tList = new List<int>();
             for (int s = 0; s < sides; s++)
             {
+                tList.Add(s);
+                tList.Add(s + 1);
+                tList.Add(s + bOffset);
 
+                tList.Add(s + bOffset);
+                tList.Add(s + 1);
+                tList.Add(s + bOffset + 1);
             }
+            for (int i = 0; i < tList.Count; i++)
+                mesh.triangles[i] = tList[i];
+            return mesh;
+        }
+
+        private static UncheckedMesh CreatePismEnds(int sides, float diameter, float length, bool inscribed)
+        {
+            var outerDiam = GetRealOuterDiam(inscribed, diameter, sides);
+            var innerDiam = GetRealInnerDiam(inscribed, diameter, sides);
+            
+            float theta = (Mathf.PI * 2f) / (float)sides;
+            theta /= 2f;
+            var oRad = outerDiam / 2f;
+            var iRad = innerDiam / 2f;
+
+            var vertices = new List<Vertex>();
+
+            vertices.Add(new Vertex(new Vector3(0, -0.5f * length, 0), new Vector3(0, -1, 0), new Vector4(-1, 0, 0, 1), new Vector2(0.5f, 0.5f)));//top center
+
+            for (int s = 0; s <= sides * 2; s++)
+            {
+                float posX = Mathf.Cos(theta * s - theta);
+                float posZ = -Mathf.Sin(theta * s - theta);
+                var radius = s % 2 == 0 ? oRad : iRad;
+
+                vertices.Add(new Vertex(
+                    new Vector3(posX * radius, -0.5f * length, posZ * radius),
+                    new Vector3(0, -1, 0),
+                    new Vector4(-1, 0, 0, 1),
+                    new Vector2((posX + 1f) / 2f, (posZ + 1f) / 2f)));
+            }
+
+            int vPerSide = vertices.Count;
+
+            vertices.Add(new Vertex(new Vector3(0, 0.5f * length, 0), new Vector3(0, 1, 0), new Vector4(-1, 0, 0, -1), new Vector2(0.5f, 0.5f)));//bottom center
+
+            for (int s = 0; s <= sides * 2; s++)
+            {
+                float posX = Mathf.Cos(theta * s - theta);
+                float posZ = -Mathf.Sin(theta * s - theta);
+                var radius = s % 2 == 0 ? oRad : iRad;
+
+                vertices.Add(new Vertex(
+                    new Vector3(posX * radius, 0.5f * length, posZ * radius),
+                    new Vector3(0, 1, 0),
+                    new Vector4(-1, 0, 0, -1),
+                    new Vector2((posX + 1f) / 2f, (posZ + 1f) / 2f)));
+            }
+            
+            var tList = new List<int>();
+
+            for (int s = 0; s < sides * 2; s++)
+            {
+                tList.Add(0);
+                tList.Add(2 + s);
+                tList.Add(1 + s);
+
+                tList.Add(vPerSide);
+                tList.Add(vPerSide + 1 + s);
+                tList.Add(vPerSide + 2 + s);
+            }
+
+            Debug.Log(string.Format("{0} sided prism cap info: vertices={1} indices={2}, triangles={3}", sides, vertices.Count, tList.Count, tList.Count / 3));
+            var mesh = new UncheckedMesh(vertices.Count, tList.Count / 3);
+
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                mesh.verticies[i] = vertices[i].Pos;
+                mesh.normals[i] = vertices[i].Norm;
+                mesh.tangents[i] = vertices[i].Tan;
+                mesh.uv[i] = vertices[i].Uv;
+            }
+
+            for (int i = 0; i < tList.Count; i++)
+                mesh.triangles[i] = tList[i];
 
             return mesh;
         }
+
+        class Vertex
+        {
+            public Vector3 Pos { get; set; }
+            public Vector3 Norm { get; set; }
+            public Vector4 Tan { get; set; }
+            public Vector2 Uv { get; set; }
+
+            public Vertex()
+            {
+                Pos = Vector3.zero;
+                Norm = Vector3.zero;
+                Tan = Vector4.zero;
+                Uv = Vector2.zero;
+            }
+
+            public Vertex(Vector3 pos, Vector3 norm, Vector4 tan, Vector2 uv)
+            {
+                Pos = pos;
+                Norm = norm;
+                Tan = tan;
+                Uv = uv;
+            }
+        }
+
 
         #endregion
 
