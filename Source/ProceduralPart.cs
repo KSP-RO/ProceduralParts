689bbda90280afad1f5ae12eab2428c8ad0be507
DRVeyl <DRVeyl@gmail.com>
2019-12-15 23:23:34 -0500

    Target volume rescaling & TU tweaks.
    Split out TechLimit to separate file.
    Better default for forceLegacyTextures setting.  Existing vessels and .craft files that do not have TU textures force on.  New parts added in the editor do not force on.

--- Source/ProceduralPart.cs
+++ Source/ProceduralPart.cs
@@ -54,6 +54,10 @@ namespace ProceduralParts
             if (HighLogic.LoadedScene == GameScenes.LOADING)
                 LoadTechLimits(node);
 
+            // An existing vessel part or .craft file that has never set this value before, but not the availablePart
+            if (HighLogic.LoadedScene != GameScenes.LOADING && !node.HasValue(nameof(forceLegacyTextures)))
+                forceLegacyTextures = true;
+
             if (HighLogic.LoadedSceneIsFlight)
             {
                 // Create a temporary collider for KSP so that it can set the craft on the ground properly
@@ -365,6 +369,8 @@ namespace ProceduralParts
         public bool allowCurveTweaking = true;
 
         private readonly List<TechLimit> techLimits = new List<TechLimit>();
+        public TechLimit currentLimit;
+        private bool needsTechInit;
 
         private void LoadTechLimits(ConfigNode node)
         {
@@ -376,9 +382,6 @@ namespace ProceduralParts
                 Debug.Log($"{ModTag} LoadTechLimits loading {limit}");
             }
         }
-
-        private bool needsTechInit;
-        public TechLimit currentLimit;
 
         private void InitializeTechLimits()
         {
@@ -422,71 +425,6 @@ namespace ProceduralParts
 
             foreach (ProceduralAbstractShape shape in GetComponents<ProceduralAbstractShape>())
                 shape.UpdateTechConstraints();
-        }
-
-        public class TechLimit : IConfigNode
-        {
-            [Persistent]
-            public string name;
-            [Persistent]
-            public float diameterMin = float.NaN;
-            [Persistent]
-            public float diameterMax = float.NaN;
-            [Persistent]
-            public float lengthMin = float.NaN;
-            [Persistent]
-            public float lengthMax = float.NaN;
-            [Persistent]
-            public float volumeMax = float.NaN;
-            [Persistent]
-            public bool allowCurveTweaking = true;
-
-            public void Load(ConfigNode node)
-            {
-                ConfigNode.LoadObjectFromConfig(this, node);
-                if (name == null)
-                {
-                    name = node.GetValue("TechRequired");
-                }
-            }
-
-            public void Save(ConfigNode node)
-            {
-                ConfigNode.CreateConfigFromObject(this, node);
-            }
-
-            internal void Validate()
-            {
-                if (diameterMax == 0)
-                    diameterMax = float.PositiveInfinity;
-                if (float.IsInfinity(diameterMin))
-                    diameterMin = 0.01f;
-                if (lengthMax == 0)
-                    lengthMax = float.PositiveInfinity;
-                if (float.IsInfinity(lengthMin))
-                    lengthMin = 0.01f;
-                if (volumeMax == 0)
-                    volumeMax = float.PositiveInfinity;
-            }
-
-            internal void ApplyLimit(TechLimit limit)
-            {
-                if (limit.diameterMin < diameterMin)
-                    diameterMin = limit.diameterMin;
-                if (limit.diameterMax > diameterMax)
-                    diameterMax = limit.diameterMax;
-                if (limit.lengthMin < lengthMin)
-                    lengthMin = limit.lengthMin;
-                if (limit.lengthMax > lengthMax)
-                    lengthMax = limit.lengthMax;
-                if (limit.volumeMax > volumeMax)
-                    volumeMax = limit.volumeMax;
-                if (limit.allowCurveTweaking)
-                    allowCurveTweaking = true;
-            }
-
-            public override string ToString() =>
-                $"TechLimits(TechRequired={name} diameter=({diameterMin:G3}, {diameterMax:G3}) length=({lengthMin:G3}, {lengthMax:G3}) volumeMax={volumeMax:G3} )";
         }
 
         private void SetFromLimit(TechLimit limit)
@@ -604,6 +542,8 @@ namespace ProceduralParts
 
             UpdateTFInterops();
         }
+
+        public bool SeekVolume(float targetVolume) => shape.SeekVolume(targetVolume);
 
         #endregion
 
