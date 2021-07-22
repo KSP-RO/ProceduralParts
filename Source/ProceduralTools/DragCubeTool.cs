9c3350ea4b0577320f155ade700d3b8a1df53142
DRVeyl <DRVeyl@gmail.com>
2021-07-21 22:09:43 -0400

    WIP: ProceduralTools Support
    Start using a unified strategy for common procedural tasks, like drag cube handling and adjusting KSPFields with UIControls from outside their PAW

---
+++ Source/ProceduralTools/DragCubeTool.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace ProceduralTools
+{
+    public class DragCubeTool : MonoBehaviour
+    {
+        public Part part;
+
+        public static DragCubeTool UpdateDragCubes(Part p, bool immediate = false)
+        {
+            var tool = p.GetComponent<DragCubeTool>();
+            if (tool == null)
+            {
+                tool = p.gameObject.AddComponent<DragCubeTool>();
+                tool.part = p;
+                if (immediate && tool.Ready())
+                    tool.UpdateCubes();
+            }
+            return tool;
+        }
+
+        public void FixedUpdate()
+        {
+            if (Ready())
+                UpdateCubes();
+        }
+
+        public bool Ready()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+                return FlightGlobals.ready; //&& !part.packed && part.vessel.loaded;
+            if (HighLogic.LoadedSceneIsEditor)
+                return part.localRoot == EditorLogic.RootPart && part.gameObject.layer != LayerMask.NameToLayer("TransparentFX");
+            return true;
+        }
+
+        private void UpdateCubes()
+        {
+            if (FARinstalled)
+                part.SendMessage("GeometryPartModuleRebuildMeshData");
+            var dragCube = DragCubeSystem.Instance.RenderProceduralDragCube(part);
+            part.DragCubes.ClearCubes();
+            part.DragCubes.Cubes.Add(dragCube);
+            part.DragCubes.ResetCubeWeights();
+            part.DragCubes.ForceUpdate(true, true, false);
+            part.DragCubes.SetDragWeights();
+            Destroy(this);
+        }
+
+        public static bool FARinstalled => AssemblyLoader.loadedAssemblies.Any(a => a.assembly.GetName().Name == "FerramAerospaceResearch");
+    }
+}
---
+++ Source/ProceduralTools/KSPFieldTool.cs
@@ -0,0 +1,23 @@
+﻿namespace ProceduralTools
+{
+    public class KSPFieldTool
+    {
+        private const System.Reflection.BindingFlags SetFieldFlags = System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance;
+        public static void SetField(PartModule instance, BaseField field, object val)
+        {
+            if (instance == null || field == null || val == null)
+                return;
+            var pai = field.uiControlEditor?.partActionItem as UIPartActionFieldItem;
+            if (pai == null)
+            {
+                UIPartActionController.Instance.SpawnPartActionWindow(instance.part);
+                pai = field.uiControlEditor?.partActionItem as UIPartActionFieldItem;
+            }
+            if (pai != null &&
+                pai.GetType().GetMethod("SetFieldValue", SetFieldFlags) is var _mi)
+            {
+                _mi.Invoke(pai, new object[] { val });
+            }
+        }
+    }
+}
