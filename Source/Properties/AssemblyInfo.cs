7c37df695bd9b44ec9491d70a248eec2888a96cd
NathanKell <nathankell@gmail.com>
2022-09-09 20:25:26 -0700

    Update to version 2.4.3

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.4.2.0")]
+[assembly: AssemblyFileVersion("2.4.3.0")]
